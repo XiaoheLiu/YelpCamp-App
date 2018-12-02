883cd95fdc3f37fa4a4aba2a2b5d273b973f4722
Athena Liu <sdathena@mail.ubc.ca>
2018-12-01 23:26:17 -0800

    Add SHOW page to display the detail information of each campground

--- app.js
+++ app.js
@@ -18,38 +18,27 @@ app.set("view engine", "ejs");
  // Create campgrounds collection
  var Campground = mongoose.model("Campground", campgroundSchema);
 
- // Creat campground entries
-
-//  Campground.create({
-//     name: "Granite Hill",
-//     image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"
-// }, function(err, campground){
-//     if (err){
-//         console.log(err);
-//     } else {
-//         console.log(campground);
-//     }
-// });
-
 app.get("/", function(req, res){
     res.render("landing");
 });
 
+// INDEX - show all campgrounds
 app.get("/campgrounds", function(req, res){
     Campground.find({}, function(err, campgrounds){
         if (err){
             console.log(err);
         } else{
-            res.render("campgrounds", {campgrounds: campgrounds});
+            res.render("index", {campgrounds: campgrounds});
         }
     });
 });
 
+//CREATE - add new campground to DB
 app.post("/campgrounds", function(req, res){
     var name = req.body.name;
     var image = req.body.image;
-    var newCampground = {name:name, image: image};
-    // campgrounds.push(newCampground);
+    var description = req.body.description;
+    var newCampground = {name:name, image: image, description: description};
     Campground.create(newCampground, function(err, newlyCreated){
         if (err){
             console.log(err);
@@ -59,8 +48,21 @@ app.post("/campgrounds", function(req, res){
     });
 });
 
+//NEW - show form to create new campground
 app.get("/campgrounds/new", function(req, res){
     res.render("new.ejs");
+});
+
+// SHOW - shows more info about one campground
+app.get("/campgrounds/:id", function(req, res){
+    // find the campground by ID
+    Campground.findById(req.params.id, function(err, foundCampground){
+        if (err){
+            console.log(err);
+        } else {
+            res.render("show.ejs", {campground: foundCampground});
+        }
+    });
 });
 
 app.listen(3000, function(){
