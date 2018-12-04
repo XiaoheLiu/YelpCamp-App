ad4440e6517e242189b41d8b358057544f651d9d
Athena Liu <sdathena@mail.ubc.ca>
2018-12-03 16:09:37 -0800

    Add gitignore file

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ app.get("/campgrounds/new", function(req, res){
 // SHOW - shows more info about one campground
 app.get("/campgrounds/:id", function(req, res){
     // find the campground by ID
-    Campground.findById(req.params.id, function(err, foundCampground){
+    Campground.findById(req.params.id).populate("comments").exec( function(err, foundCampground){
         if (err){
             console.log(err);
         } else {
